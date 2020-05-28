89ef731c74dc64657a5381678c9a8ededf467ff5
moraes-caroline <moraes.caroline@hotmail.com>
2020-05-28 16:16:46 -0300

    atualizando arquivos

---
+++ Aula 7 - Armazenando dados no SQLite/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -0,0 +1,76 @@
+package com.example.android.waitlist;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.waitlist.data.WaitlistContract;
+
+
+public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
+
+    private Cursor mCursor;
+    private Context mContext;
+
+    /**
+     * Constructor using the context and the db cursor
+     * @param context the calling context/activity
+     * @param cursor the db cursor with waitlist data to display
+     */
+    public GuestListAdapter(Context context, Cursor cursor) {
+        this.mContext = context;
+        this.mCursor = cursor;
+    }
+
+    @Override
+    public GuestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Get the RecyclerView item layout
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.guest_list_item, parent, false);
+        return new GuestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(GuestViewHolder holder, int position) {
+        if (!mCursor.moveToPosition(position))
+            return; // bail if returned null
+        String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
+        int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
+        holder.nameTextView.setText(name);
+        holder.partySizeTextView.setText(String.valueOf(partySize));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+    
+    /**
+     * Inner class to hold the views needed to display a single item in the recycler-view
+     */
+    class GuestViewHolder extends RecyclerView.ViewHolder {
+
+        // Will display the guest name
+        TextView nameTextView;
+        // Will display the party size number
+        TextView partySizeTextView;
+
+        /**
+         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
+         * TextViews
+         *
+         * @param itemView The View that you inflated in
+         *                 {@link GuestListAdapter#onCreateViewHolder(ViewGroup, int)}
+         */
+        public GuestViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+            partySizeTextView = (TextView) itemView.findViewById(R.id.party_size_text_view);
+        }
+
+    }
+}
